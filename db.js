7bd2b62d3636ccee7bf2c5ae7d5b8a061dd5581e
ShaheryarRafique <shaheryar.rafique100@gmail.com>
2023-05-11 23:59:21 +0500

    update dotenv path

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({path: '../config.env'});
+dotenv.config({path: './config.env'});
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
